c1926515e2485339e5147d890afdc2f7a3532d2a
kirtikumar8536 <kirtikumar8536@gmail.com>
2024-01-18 01:14:23 +0530

    pagination and sort

--- src/main/java/com/example/AmigoJpaApplication.java
+++ src/main/java/com/example/AmigoJpaApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -20,73 +23,33 @@ public class AmigoJpaApplication {
     @Bean
     public CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-           /* Student maria = new Student("Maria",
-                    "Jones",
-                    "maria@gmail.com",
-                    32);
-            Student maya = new Student("maya",
-                    "Rob",
-                    "maya.Rob@gmail.com",
-                    22);
-            Student martha = new Student("martha",
-                    "Jones",
-                    "martha@gmail.com",
-                    18);
-            Student Brandon = new Student("Brandon",
-                    "mecullum",
-                    "mecullum.Brandon@gmail.com",
-                    26);
-            Student maria2 = new Student("Maria",
-                    "mecullum",
-                    "mecullum.maria@gmail.com",
-                    26);
-            //studentRepository.save(maria);
-            System.out.println("================Adding maria and maya===========================");
-            studentRepository.saveAll(List.of(maria, maya,martha,Brandon,maria2));
-		*//*	System.out.println("=======================No of Students==========================");
-			System.out.println(studentRepository.count());//counting total tuples/people
-
-			studentRepository.findById(2L).ifPresentOrElse(System.out::println,
-					()->{
-						System.out.println("student with id 2 not found");
-					});
-			studentRepository.findById(3L).ifPresentOrElse(System.out::println,
-					()->{
-						System.out.println("student with id 3 not found");
-					});
-			System.out.println("=======================Select All Students:==========================");
-			List<Student>students=studentRepository.findAll();
-			students.forEach(System.out::println);
-			System.out.println("=======================delete id 2: ==========================");
-			studentRepository.deleteById(2L);
-			System.out.println("=======================No of Students:==========================");
-			System.out.println(studentRepository.count());*//*
-            studentRepository
-                    .findStudentByEmail("maria@gmail.com").
-                    ifPresentOrElse(System.out::println,
-                            () -> System.out.println("Student with email maria@gmail.com is not found"));
-            System.out.println("======================================");
-            studentRepository.
-                    selectStudWherefNameAndAgeGreaterOrEqual
-                            ("Maria",18).forEach(System.out::println);
-            System.out.println("==============using native=true========================");
-            studentRepository.
-                    selectStudWherefNameAndAgeGreaterOrEqualNative
-                            ("Maria",18).forEach(System.out::println);
-
-            System.out.println("============deleting maria with id=5===========");
-            System.out.println(studentRepository.deleteStudentById(5L));*/
-            Faker faker = new Faker();
-            for (int i = 0; i < 20; i++) {
-                String firstName = faker.name().firstName();
-                String lastName = faker.name().lastName();
-                String email = String.format("%s.%s@helper.in", firstName, lastName);
-                Student student = new Student(firstName, lastName, email,
-                        faker.number().numberBetween(18, 50));
-                studentRepository.save(student);
-            }
+            generateRandomStudents(studentRepository);
+            // Sort sort = Sort.by(Sort.Direction.ASC, "firstName");
+            PageRequest pageRequest = PageRequest.of(0, 5, Sort.by("firstName").ascending());
+            Page<Student> page = studentRepository.findAll(pageRequest);
+            System.out.println(page);
         };
     }
+
+    private  void sorting(StudentRepository studentRepository) {
+        Sort sort = Sort.by("firstName").ascending()
+                .and(Sort.by("age").ascending());
+        studentRepository.
+                findAll(sort).forEach(student -> System.out.println(student.getFirstName()+" "+student.getAge()));
+    }
+
+    private void generateRandomStudents(StudentRepository studentRepository) {
+        Faker faker = new Faker();
+        for (int i = 0; i < 25; i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@helper.in", firstName, lastName);
+            Student student = new Student(firstName, lastName, email,
+                    faker.number().numberBetween(18, 50));
+            studentRepository.save(student);
+        }
+    }
+
 }
 
 
