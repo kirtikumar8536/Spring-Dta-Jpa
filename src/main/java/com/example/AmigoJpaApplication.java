a7fc8635f2eff7b7352e66208e6e5d964c784d2b
kirtikumar8536 <kirtikumar8536@gmail.com>
2024-01-21 11:24:03 +0530

    branch5 1

--- src/main/java/com/example/AmigoJpaApplication.java
+++ src/main/java/com/example/AmigoJpaApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.Entity.Book;
 import com.example.Entity.Student;
 import com.example.Entity.StudentIdCard;
 import com.example.Repository.StudentIdCardRepository;
@@ -13,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootApplication
@@ -26,11 +28,7 @@ public class AmigoJpaApplication {
     public CommandLineRunner commandLineRunner(StudentRepository studentRepository,
                                                StudentIdCardRepository studentIdCardRepository) {
         return args -> {
-            /*generateRandomStudents(studentRepository);
-            // Sort sort = Sort.by(Sort.Direction.ASC, "firstName");
-            PageRequest pageRequest = PageRequest.of(0, 5, Sort.by("firstName").ascending());
-            Page<Student> page = studentRepository.findAll(pageRequest);
-            System.out.println(page);*/
+
             Faker faker = new Faker();
             String firstName = faker.name().firstName();
             String lastName = faker.name().lastName();
@@ -38,26 +36,30 @@ public class AmigoJpaApplication {
             Student student = new Student(firstName, lastName, email,
                     faker.number().numberBetween(18, 50));
             StudentIdCard studentIdCard = new StudentIdCard("123456789", student);
+            student.addBook(
+                    new Book("clean code ", LocalDateTime.now().minusDays(4)));
+            student.addBook(
+                    new Book("Think grow ", LocalDateTime.now()));
+            student.addBook(
+                    new Book("spring code ", LocalDateTime.now().minusYears(1)));
+
+            //Here im not using BookRepository still im able to save the book data due to Cascade
             //Here im not using student repo to save Student property data
             studentIdCardRepository.save(studentIdCard);
-            // by default OneToOne mapping uses FetchType.EAGER so that here student object will be load here
-            studentIdCardRepository.findById(1L).
+            studentRepository.findById(1L).
                     ifPresent(System.out::println);
-            // by default OneToOne mapping uses FetchType.EAGER so that here studentIdCard object will be load here
-            studentRepository.findById(1L)
-                    .ifPresent(System.out::println);
-/*
-            Hibernate: select s1_0.id,s1_0.age,s1_0.email,s1_0.first_name,s1_0.last_name,sic1_0.id,sic1_0.card_number from student s1_0 left join student_id_card sic1_0 on s1_0.id=sic1_0.student_id where s1_0.id=?
-            Student{id=1, firstName='Alan', lastName='Quigley', email='Alan.Quigley@helper.in', age=29}
-            Hibernate: select sic1_0.id,sic1_0.card_number,s1_0.id,s1_0.age,s1_0.email,s1_0.first_name,s1_0.last_name from student_id_card sic1_0 left join student s1_0 on s1_0.id=sic1_0.student_id where sic1_0.id=?
-            StudentIdCard{id=1, cardNumber='123456789', student=Student{id=1, firstName='Alan', lastName='Quigley', email='Alan.Quigley@helper.in', age=29}}
-*/
-            System.out.println("============");
-           /* select s1_0.id,s1_0.age,s1_0.email,s1_0.first_name,s1_0.last_name,sic1_0.id,sic1_0.card_number from student s1_0
-            left join student_id_card sic1_0 on s1_0.id=sic1_0.student_id where s1_0.id=?*/
-//            Hibernate: delete from student_id_card where id=?
-//            Hibernate: delete from student where id=?
-            studentRepository.deleteById(1L);
+            studentRepository.findById(1L).
+                    ifPresent(s->{
+                        System.out.println("fetch Book Lazy.....");
+                        List<Book> books = student.getBooks();
+                        books.forEach(book -> {
+                            System.out.println(s.getFirstName()+" -borrowed- "+book.getBookName());
+                        });
+                    });
+
+            // by default OneToOne mapping uses FetchType.EAGER so that here student object will be load here
+//            studentIdCardRepository.findById(1L).
+//                    ifPresent(System.out::println);
 
         };
     }
--- src/main/java/com/example/Entity/Student.java
+++ src/main/java/com/example/Entity/Student.java
@@ -2,38 +2,56 @@ package com.example.Entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 @Entity(name = "Students")  //default name will be class name// this name will used in jpql
 @Table(name = "student",//
-       uniqueConstraints = {
-        @UniqueConstraint(name="student_email_unique",columnNames = "email")
-})
+        uniqueConstraints = {
+                @UniqueConstraint(name = "student_email_unique", columnNames = "email")
+        })
 public class Student {
     @Id
     @SequenceGenerator(name = "student_sequence",
-                        sequenceName = "student_sequence",
-                         allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "student_sequence")
-    @Column(name="id",updatable = false)
+            sequenceName = "student_sequence",
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+    @Column(name = "id", updatable = false)
     private Long id;
-    @Column(name="first_name",nullable = false,columnDefinition = "TEXT")
+    @Column(name = "first_name", nullable = false, columnDefinition = "TEXT")
     private String firstName;
-    @Column(name="last_name",nullable = false,columnDefinition = "TEXT")
+    @Column(name = "last_name", nullable = false, columnDefinition = "TEXT")
     private String lastName;
-    @Column(name="email",nullable = false,columnDefinition = "TEXT")
+
+    @Column(name = "email", nullable = false, columnDefinition = "TEXT")
     private String email;
-    @Column(name="age",nullable = false)
+    @Column(name = "age", nullable = false)
     private Integer age;
-    @OneToOne(mappedBy = "student",orphanRemoval = true) //this student comes from StudentIdCard property.
+    @OneToOne(mappedBy = "student", orphanRemoval = true) //this student comes from StudentIdCard property.
     private StudentIdCard studentIdCard;
+
+    @OneToMany(mappedBy = "student",//comes from Book field(student)
+            orphanRemoval = true, cascade = {CascadeType.PERSIST,
+            CascadeType.REMOVE},fetch = FetchType.LAZY)//default is Lazy
+    private List<Book> books = new ArrayList<>();
 
     public Student() {
     }
 
-    public Student( String firstName, String lastName, String email, Integer age) {
+    public Student(String firstName, String lastName, String email, Integer age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.age = age;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
     }
 
     public Long getId() {
@@ -48,12 +66,22 @@ public class Student {
         return lastName;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
     public Integer getAge() {
         return age;
+    }
+
+    public void addBook(Book book) {
+        if (!this.books.contains(book)) {
+            this.books.add(book);
+            book.setStudent(this);
+        }
+    }
+
+    public void removeBook(Book book) {
+        if (this.books.contains(book)) {
+            this.books.remove(book);
+            book.setStudent(null);
+        }
     }
 
     @Override
